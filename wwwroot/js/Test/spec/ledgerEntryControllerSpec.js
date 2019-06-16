9de34b670e2f49f9a7892b2918f7ce9dc891e59c
Sergio Ángel Verbo <seranfuen@gmail.com>
2019-06-16 21:12:49 +0200

    Integrating with angular.js to make the application more interactive, by being able to enter more complex journal entries as in most accounting software

--- wwwroot/js/Test/spec/ledgerEntryControllerSpec.js
+++ wwwroot/js/Test/spec/ledgerEntryControllerSpec.js
@@ -1,36 +1,153 @@
-﻿// one first test, we can run from here
-
-describe("LedgerEntryController",
+﻿describe("LedgerEntryController",
     function() {
 
         beforeEach(module("contabilidadApp"));
 
-        var $controller;
+        var $controller, $rootScope;
 
-        beforeEach(inject(function(_$controller_) {
+        beforeEach(inject(function(_$controller_, _$rootScope_) {
             $controller = _$controller_;
+            $rootScope = _$rootScope_;
         }));
 
-        describe("initial elements in lines",
+        describe("Lines",
             function() {
                 var $scope, controller;
 
                 beforeEach(function() {
-                    $scope = {};
+                    $scope = $rootScope.$new();
                     controller = $controller("LedgerEntryController", { $scope: $scope });
                 });
 
-                it("When entering new, it has one entry by default with no data",
+                it("When creating new controller, it has one entry by default with no data",
                     function() {
                         expect($scope.Lines.length).toEqual(1);
                     });
+            });
 
-                it("It has defined null properties",
+        describe("UpdateTotals",
+            function () {
+                var $scope, controller;
+
+                beforeEach(function () {
+                    $scope = $rootScope.$new();
+                    controller = $controller("LedgerEntryController", { $scope: $scope });
+                });
+
+                it("The totals line is 0 for both credit and debit if no elements exist",
+                    function () {
+                        $scope.Lines = [];
+                        $scope.updateTotals();
+                        expect($scope.TotalsLine.CreditSum).toEqual(0);
+                        expect($scope.TotalsLine.DebitSum).toEqual(0);
+                    });
+
+                it("The totals line is 0 for both credit and debit if element is undefined",
+                    function () {
+                        $scope.Lines = [{}];
+                        $scope.updateTotals();
+                        expect($scope.TotalsLine.CreditSum).toEqual(0);
+                        expect($scope.TotalsLine.DebitSum).toEqual(0);
+                    });
+
+                it("The totals line is the sum for both debit and credit",
+                    function () {
+                        $scope.Lines = [
+                            {
+                                Debit: 300,
+                                Credit: 0
+                            },
+                            {
+                                Debit: 100,
+                                Credit: 500
+                            }
+                        ];
+                        $scope.updateTotals();
+                        expect($scope.TotalsLine.CreditSum).toEqual(500);
+                        expect($scope.TotalsLine.DebitSum).toEqual(400);
+                    });
+
+                it("The totals line is parse to integer",
+                    function () {
+                        $scope.Lines = [
+                            {
+                                Debit: "300",
+                                Credit: 0
+                            },
+                            {
+                                Debit: 100,
+                                Credit: "500"
+                            }
+                        ];
+                        $scope.updateTotals();
+                        expect($scope.TotalsLine.CreditSum).toEqual(500);
+                        expect($scope.TotalsLine.DebitSum).toEqual(400);
+                    });
+
+            });
+
+        describe("fillAccount",
+            function() {
+
+                var $scope, controller;
+
+                beforeEach(function() {
+                    $scope = $rootScope.$new();
+                    controller = $controller("LedgerEntryController", { $scope: $scope });
+                });
+
+
+                it("420 becomes 420000000 (padded to complete 9 digits)",
                     function() {
-                        expect($scope.Lines[0].Account).toBeNull();
-                        expect($scope.Lines[0].Debit).toBeNull();
-                        expect($scope.Lines[0].Credit).toBeNull();
-                        expect($scope.Lines[0].Remarks).toBeNull();
+                        $scope.Lines[0].Account = "420";
+                        $scope.fillAccount(0);
+                        expect($scope.Lines[0].Account).toEqual("420000000");
+                    }
+                );
+
+                it("420.1 becomes 420000001",
+                    function() {
+                        $scope.Lines[0].Account = "420.1";
+                        $scope.fillAccount(0);
+                        expect($scope.Lines[0].Account).toEqual("420000001");
+                    });
+
+                it("420.1005 becomes 420001005",
+                    function() {
+                        $scope.addNewLine();
+                        $scope.Lines[1].Account = "420.1005";
+                        $scope.fillAccount(1);
+                        expect($scope.Lines[1].Account).toEqual("420001005");
+                    });
+
+                it("420000000 is unchanged",
+                    function () {
+                        $scope.addNewLine();
+                        $scope.Lines[1].Account = "420000000";
+                        $scope.fillAccount(1);
+                        expect($scope.Lines[1].Account).toEqual("420000000");
+                    });
+
+
+                it("Empty string is unchanged",
+                    function() {
+                        $scope.Lines[0].Account = "";
+                        $scope.fillAccount(0);
+                        expect($scope.Lines[0].Account).toEqual("");
+                    });
+
+                it("Account does not match regex /^[0-9]+\.?[0-9]+$/ is left unchanged",
+                    function() {
+                        $scope.Lines[0].Account = "420.0.1";
+                        $scope.fillAccount(0);
+                        expect($scope.Lines[0].Account).toEqual("420.0.1");
+                    });
+
+                it("Account is longer than 9 with no dot, left unchanged",
+                    function() {
+                        $scope.Lines[0].Account = "4200000000";
+                        $scope.fillAccount(0);
+                        expect($scope.Lines[0].Account).toEqual("4200000000");
                     });
             });
 
@@ -39,7 +156,7 @@ describe("LedgerEntryController",
                 var $scope, controller;
 
                 beforeEach(function() {
-                    $scope = {};
+                    $scope = $rootScope.$new();
                     controller = $controller("LedgerEntryController", { $scope: $scope });
                 });
 
--- wwwroot/js/contabilidadApp.js
+++ wwwroot/js/contabilidadApp.js
@@ -4,15 +4,44 @@ app.controller("LedgerEntryController",
     function($scope) {
         // We add a new line (ledger entry, apunte contable)
         $scope.addNewLine = function() {
-            $scope.Lines.push({
-                Account: null,
-                Debit: null,
-                Credit: null,
-                Remarks: null
-            });
+            $scope.Lines.push({});
         };
 
-        // for user convenience, by default show already one line
+        // Make a short account code have 9 digits exactly
+        // If no dot is present, right pad with zeros
+        // If present, left pad up to the digits after the dot
+        $scope.fillAccount = function(index) {
+            var code = $scope.Lines[index].Account;
+            if (!code || code.length > 9) return;
+            if (!code.match(/^[0-9]+\.?[0-9]+$/)) return;
+
+            var rightSide = code.slice(code.indexOf(".") + 1);
+            var leftSide = code.substr(0, code.indexOf("."));
+
+            if (leftSide.length === 0) {
+                leftSide = rightSide;
+                rightSide = "";
+            }
+            $scope.Lines[index].Account = leftSide + rightSide.padStart(9 - leftSide.length, "0");
+        };
+
+        $scope.updateTotals = function() {
+            $scope.TotalsLine.DebitSum = $scope.Lines.reduce(function (total, line) {
+                    return total + (parseInt(line.Debit) || 0);
+                },
+                0);
+            $scope.TotalsLine.CreditSum = $scope.Lines.reduce(function(total, line) {
+                    return total + (parseInt(line.Credit) || 0);
+                },
+                0);
+        };
+
         $scope.Lines = [];
+
+        $scope.TotalsLine = {
+            DebitSum: 0,
+            CreditSum: 0
+        };
+
         $scope.addNewLine();
     });
